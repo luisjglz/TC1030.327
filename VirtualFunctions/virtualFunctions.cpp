4a1a6b91d2da241e281f2fa08097c833a55dda6a
Luis José González <68867942+luisjglz@users.noreply.github.com>
2023-06-02 11:50:54 -0600

    Virtual functions

---
+++ VirtualFunctions/virtualFunctions.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+class Shape{
+    public:
+        Shape(){
+            cout << "Shape created" << endl;
+        }
+        void draw(){
+            cout << "Drawing shape" << endl;
+        }
+        void print(){
+            cout << "Printing shape" << endl;
+        }
+};
+
+class Square : public Shape{
+    public:
+        Square() : Shape(){
+            cout << "Square created" << endl;
+        }
+        void draw(){
+            cout << "Drawing square" << endl;
+        }
+        void print(){
+            cout << "Printing square" << endl;
+        }
+};
+
+int main(){
+    Shape * shapePtr;
+    Square sq;
+
+    shapePtr = & sq;
+
+    shapePtr->draw();
+
+
+    return 0;
+}
